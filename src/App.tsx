71240c87644e93ac1b0ff155da9e0c4c0b48eba3
ZAMBAR <zambar@163.com>
2025-02-24 18:51:52 +0800

    fix: using local package

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useCallback, useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Button, setDarkModeActivation } from 'nes-ui-react'
 
@@ -17,14 +15,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1 className="text-purple-300 text-8xl">Vite + React</h1>
       <div className="card">
         <Button onClick={() => setCount((count) => count + 1)}>
