028c25e9c1469baa7e4bd0320c1eb9f8057c16d0
ZAMBAR <zambar@163.com>
2025-02-24 19:43:24 +0800

    feat: added more components

--- src/gallery.tsx
+++ src/gallery.tsx
@@ -1,4 +1,6 @@
-import { A, Badge, BadgeSplitted, BlockText, Button } from "nes-ui-react";
+import { A, Badge, BadgeSplitted, BlockText, Button, Checkbox, IconButton, Input, Radio } from "nes-ui-react";
+import { SVGProps } from "react";
+
 
 export default function gallery() {
   return (
@@ -7,8 +9,36 @@ export default function gallery() {
       <A className="text-xl text-inherit" href="#">Link</A>
       <Badge text="Badge - SmallText" backgroundColor="warning" className="m-auto w-[200px]"/>
       <BadgeSplitted textLeft="Badge" text="Splitted" backgroundColor="success" className="text-xl m-3"/>
-      <BlockText text="BlockText"/>
-
+      <IconButton color="success">
+        <Android className="w-10 h-10"/>
+        <span className="text-2xl">Android</span>
+      </IconButton>
+      <div className="flex gap-3 text-lg">
+        <Radio name="radio" value="1" label="Radio Checked" checked={true}/>
+        <Radio name="radio" value="2" label="Radio Uncheked"/>
+      </div>
+      <div className="flex gap-3 text-lg">
+        <Checkbox label="Checkbox Checked" checked={true}/>
+        <Checkbox label="Checkbox Unchecked"/>
+      </div>
+      <Input type="text" color="warning" className="text-md bg-inherit" value="Input"/>
     </div>
+  )
+}
+
+export function Android(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M2 5h2v2H2zm4 4H4V7h2zm2 0H6v2H4v2H2v6h20v-6h-2v-2h-2V9h2V7h2V5h-2v2h-2v2h-2V7H8zm0 0h8v2h2v2h2v4H4v-4h2v-2h2zm2 4H8v2h2zm4 0h2v2h-2z"
+      ></path>
+    </svg>
   )
 }
