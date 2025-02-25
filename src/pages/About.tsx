b61d44c28e3cfc58f8be7a157c90784a184793f6
zambar <zambar@163.com>
2025-02-25 19:46:23 +0800

    feat: easteregg meow text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,12 +2,27 @@
 import { PixelBorder } from "nes-ui-react";
 import Nyan from "../assets/nyan.webp";
 import "../App.css";
+import { useEffect, useState } from "react";
 
 export default function About() {
+  const [meowText, setMeowText] = useState("=^·.·^=")
+
+  // setTimeout
+  useEffect(() => {
+    const meowTexts = [
+      "=^·.·^=",
+      "=^ -.- ^=",
+    ]
+    const interval = setInterval(() => {
+      setMeowText(meowTexts[Math.floor(Math.random() * meowTexts.length)])
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div className="flex gap-3 items-center justify-center flex-col">
       <h1 className="text-5xl sm:text-8xl">CatLin</h1>
-      <h2 className="text-2xl py-3">=^·.·^=</h2>
+      <h2 className="text-2xl py-3">{meowText}</h2>
       <PixelBorder doubleSize doubleRoundCorners>
         <img src={Nyan} alt="Nyan Cat" className="w-full h-full" />
       </PixelBorder>
