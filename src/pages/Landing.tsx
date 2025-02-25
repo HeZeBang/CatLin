b5d6e9c1c6c0fd0fbaf5b623c9d21ebefa6e430f
zambar <zambar@163.com>
2025-02-25 09:55:38 +0800

    test: sample ddl item

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,16 +1,25 @@
 import { PixelBorder } from "nes-ui-react";
-import Nyan from "../assets/nyan.webp";
 
 export default function Landing() {
   return (
-    <div className="flex gap-3 items-center justify-center flex-col">
-      <h1 className="text-5xl sm:text-8xl">CatLin</h1>
-      <h2 className="text-2xl py-3">=＾● ⋏ ●＾=</h2>
-      <p className="text-xl">
-        让 CatLin 陪伴你的每一天！
-      </p>
-      <PixelBorder doubleSize doubleRoundCorners>
-        <img src={Nyan} alt="Nyan Cat" className="w-full h-full"/>
+    <div className="flex gap-3 items-center justify-center flex-col w-full max-w-md">
+      <PixelBorder doubleRoundCorners className="w-full">
+        <button className="nes-ui-icon-btn nes-ui-btn-success nes-ui-is-size-medium w-full">
+          <div className="p-2 flex gap-3 items-center w-full">
+            <div className="flex-1 flex flex-col items-start">
+              <span className="text-xl">CourseName</span>
+              <span>HomeworkName</span>
+            </div>
+            <div className="flex items-end flex-col">
+              <div className="flex gap-1">
+                <span className="text-4xl">3<sup className="text-sm">d</sup></span>
+                <span className="text-4xl">2<sup className="text-sm">h</sup></span>
+                <span className="text-4xl">1<sup className="text-sm">m</sup></span>
+              </div>
+              <span>Due: 2025/02/14 00:00</span>
+            </div>
+          </div>
+        </button>
       </PixelBorder>
     </div>
   )
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,4 +1,18 @@
+import { PixelBorder } from "nes-ui-react";
+import Nyan from "../assets/nyan.webp";
+import "../App.css";
+
 export default function Settings() {
-    return <>
-    </>
+  return (
+    <div className="flex gap-3 items-center justify-center flex-col">
+      <h1 className="text-5xl sm:text-8xl">CatLin</h1>
+      <h2 className="text-2xl py-3">=＾● ⋏ ●＾=</h2>
+      <PixelBorder doubleSize doubleRoundCorners>
+        <img src={Nyan} alt="Nyan Cat" className="w-full h-full"/>
+      </PixelBorder>
+      <p className="text-2xl items-center flex gap-3">
+        Made by C4TL1N with <i className="nes-icon is-medium heart"/>
+      </p>
+    </div>
+  )
 }
