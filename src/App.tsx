6724db397fc0901729f7e630474e812b081d4742
zambar <zambar@163.com>
2025-03-20 19:10:05 +0800

    feat: drawer test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useCallback, useEffect, useState } from 'react'
 import './App.css'
 import { IconButton, Progress, setDarkModeActivation } from 'nes-ui-react'
-import { NavLink, Outlet } from 'react-router'
+import { NavLink, Outlet, useLocation } from 'react-router'
 import { Sun, Moon, MenuIcon } from './components/Icons'
 
 function App() {
   const [darkMode, setDarkMode] = useState(false)
   const [menuOpen, setMenuOpen] = useState(false)
+  const location = useLocation()
 
   const [progressVal, setProgressVal] = useState(0)
 
@@ -30,6 +31,9 @@ function App() {
 
   //   return () => clearInterval(interval);
   // })
+  useEffect(() => {
+    console.log(location)
+  }, [location])
 
   const NavigatorItems = () => (
     <>
@@ -41,38 +45,36 @@ function App() {
   )
 
   return (
-    <div>
-      <div className="sticky w-screen left-0 top-0 z-50 p-3 bg-auto toolbar"
-        style={{
-          borderBottom: "4px solid #D3D3D3"
-        }}
-      >
-        <div className="max-w-lg flex mx-auto items-center">
-          <span className="text-xl flex-1">CatLin <sup className="text-sm">=^·.·^=</sup></span>
-          <nav className="md:flex gap-3 hidden mr-3">
+    <div className="h-screen">
+      <div className="sticky w-screen left-0 top-0 z-50">
+        <div className="w-screen left-0 top-0 z-50 p-3 bg-auto toolbar">
+          <div className="max-w-lg flex mx-auto items-center">
+            <span className="text-xl flex-1">CatLin <sup className="text-sm">=^·.·^=</sup></span>
+            <nav className="md:flex gap-3 hidden mr-3">
+              <NavigatorItems />
+            </nav>
+            <IconButton color="warning" borderInverted onClick={toggleDarkMode}>
+              {darkMode ? <Sun /> : <Moon />}
+            </IconButton>
+            <IconButton color="primary" borderInverted className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+              <MenuIcon />
+            </IconButton>
+          </div>
+          <div className={`max-w-lg mx-auto items-center justify-end mt-1 mr-3 gap-3 ${menuOpen ? 'flex md:hidden' : 'hidden'}`}>
             <NavigatorItems />
-          </nav>
-          <IconButton color="warning" borderInverted onClick={toggleDarkMode}>
-            {darkMode ? <Sun /> : <Moon />}
-          </IconButton>
-          <IconButton color="primary" borderInverted className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
-            <MenuIcon />
-          </IconButton>
+          </div>
         </div>
-        <div className={`max-w-lg mx-auto items-center justify-end mt-1 mr-3 gap-3 ${menuOpen ? 'flex md:hidden' : 'hidden'}`}>
-          <NavigatorItems />
-        </div>
+        <Progress value={progressVal} max="100" color="primary"
+          style={{
+            margin: "0",
+            height: "8px",
+            display: "flex",
+            // opacity: progressVal === 0? '0%':'100%',
+            transition: "opacity 200ms ease",
+          }}
+        />
       </div>
-      <Progress value={progressVal} max="100" color="primary"
-        style={{
-          margin: "0",
-          height: "8px",
-          display: "flex",
-          opacity: progressVal === 0? '0%':'100%',
-          transition: "opacity 200ms ease",
-        }}
-      />
-      <div className="flex gap-3 items-center justify-center flex-col m-3">
+      <div className={`flex gap-3 items-center justify-center flex-col ${location.pathname === '/' ? 'm-0 h-auto' : 'm-3'}`}>
         <Outlet />
       </div>
     </div>
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,13 +2,21 @@ import { useEffect, useRef, useState } from "react";
 import { HomeworkItem, HwItem } from "../components/HomeworkUtils";
 import { AccountType, LoadHomework, LoadUsername } from "../components/Utils";
 import { Link } from "react-router";
-import { Button } from "nes-ui-react";
+import { Button, Spacer, Toolbar } from "nes-ui-react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 export default function Landing() {
   const [homeworks, setHomeworks] = useState<HomeworkItem[]>([])
   const [firstUse, setFirstUse] = useState(true)
   const [dueSplit, setDueSplit] = useState(0);
   const bottomRef = useRef<HTMLDivElement>(null);
+
+  const { unityProvider, sendMessage } = useUnityContext({
+    loaderUrl: "unity/demo/demo.loader.js",
+    dataUrl: "unity/demo/demo.data.br",
+    frameworkUrl: "unity/demo/demo.framework.js.br",
+    codeUrl: "unity/demo/demo.wasm.br",
+  })
 
   useEffect(() => {
     var hwTemp = [] as HomeworkItem[]
@@ -26,63 +34,79 @@ export default function Landing() {
   }, [dueSplit])
 
   return (
-    <div className="flex gap-3 items-center justify-center flex-col w-full max-w-md">
-      {firstUse ? (
-        <div className="flex flex-col gap-3 mt-5 items-center justify-center">
-          <h1 className="text-5xl sm:text-6xl flex">=<span className="animate-bounce">^</span>·.·<span className="animate-bounce">^</span>=</h1>
-          <span className="text-2xl">欢迎来到 CatLin ！</span>
-          <span className="text-base mb-5">完成作业也可如此有趣！</span>
-          <span className="text-base">
-            快到
-            {' '}
-            <Link to="/settings" className="text-blue-500 animate-pulse">设置页</Link>
-            {' '}
-            关联作业账户吧！</span>
+    <>
+      <Unity unityProvider={unityProvider} className='fixed w-full h-full m-0 top-0 left-0 z-0' />
+      <div className="w-full h-auto overflow-scroll drawer flex items-center justify-start flex-col z-10 px-5"
+        style={{
+          marginTop: "calc(-9em + 100vh)",
+          minHeight: "100vh",
+        }}
+      >
+        <div className="nes-ui-toolbar -mx-5 w-full mb-5 mt-2"
+          style={{
+            minHeight: "2em",
+          }}>
+          <span className="text-xl w-full justify-center">TODOs</span>
         </div>
-      ) : (
-        <>
-          {homeworks
-            .filter(item => item.due >= dueSplit)
-            .filter(item => !item.submitted)
-            .length == 0 && (
-              <div className="flex flex-col gap-5 mt-5 items-center justify-center">
-                <h1 className="text-4xl sm:text-6xl flex"><span className="animate-bounce">¯\_</span>(ツ)<span className="animate-bounce">_/¯</span></h1>
-                <span className="text-base sm:text-xl">你似乎已经把作业全写完了呢！</span>
+        <div className="flex gap-3 items-center justify-center flex-col w-full max-w-md">
+          {firstUse ? (
+            <div className="flex flex-col gap-3 mt-5 items-center justify-center">
+              <h1 className="text-5xl sm:text-6xl flex">=<span className="animate-bounce">^</span>·.·<span className="animate-bounce">^</span>=</h1>
+              <span className="text-2xl">欢迎来到 CatLin ！</span>
+              <span className="text-base mb-5">完成作业也可如此有趣！</span>
+              <span className="text-base">
+                快到
+                {' '}
+                <Link to="/settings" className="text-blue-500 animate-pulse">设置页</Link>
+                {' '}
+                关联作业账户吧！</span>
+            </div>
+          ) : (
+            <>
+              {homeworks
+                .filter(item => item.due >= dueSplit)
+                .filter(item => !item.submitted)
+                .length == 0 && (
+                  <div className="flex flex-col gap-5 mt-5 items-center justify-center">
+                    <h1 className="text-4xl sm:text-6xl flex"><span className="animate-bounce">¯\_</span>(ツ)<span className="animate-bounce">_/¯</span></h1>
+                    <span className="text-base sm:text-xl">你似乎已经把作业全写完了呢！</span>
+                  </div>
+                )}
+              {(
+                homeworks
+                  .filter(item => item.due >= dueSplit)
+                  // .filter(item => !item.submitted)
+                  .map((hw, _) => (
+                    <HwItem
+                      key={`${hw.course}-s${hw.title}`}
+                      course={hw.course}
+                      title={hw.title}
+                      submitted={hw.submitted}
+                      due={hw.due}
+                      url={hw.url}
+                    />
+                  ))
+              )}
+              <div className="flex gap-3 items-center justify-center">
+                <Button
+                  onClick={() => { setDueSplit(due => due - 7 * 24 * 60 * 60) }}
+                  className="text-nowrap"
+                >
+                  -↓-
+                </Button>
+                <span>截至 {new Date(dueSplit * 1000).toLocaleString()}</span>
+                <Button
+                  onClick={() => { setDueSplit(due => Math.min(Date.now() / 1000, due + 7 * 24 * 60 * 60)) }}
+                  className="text-nowrap"
+                >
+                  -↑-
+                </Button>
               </div>
-            )}
-          {(
-            homeworks
-              .filter(item => item.due >= dueSplit)
-              // .filter(item => !item.submitted)
-              .map((hw, _) => (
-                <HwItem
-                  key={`${hw.course}-s${hw.title}`}
-                  course={hw.course}
-                  title={hw.title}
-                  submitted={hw.submitted}
-                  due={hw.due}
-                  url={hw.url}
-                />
-              ))
+              <div ref={bottomRef} />
+            </>
           )}
-          <div className="flex gap-3 items-center justify-center">
-            <Button
-              onClick={() => { setDueSplit(due => due - 7 * 24 * 60 * 60) }}
-              className="text-nowrap"
-            >
-              -↓-
-            </Button>
-            <span>截至 {new Date(dueSplit * 1000).toLocaleString()}</span>
-            <Button
-              onClick={() => { setDueSplit(due => Math.min(Date.now() / 1000, due + 7 * 24 * 60 * 60)) }}
-              className="text-nowrap"
-            >
-              -↑-
-            </Button>
-          </div>
-          <div ref={bottomRef} />
-        </>
-      )}
-    </div>
+        </div>
+      </div>
+    </>
   )
 }
