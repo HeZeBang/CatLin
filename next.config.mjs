07a2e2ac583761634b5010f27e374d97a578b92d
zambar <zambar@163.com>
2025-04-18 05:23:42 +0800

    fix: distdir cancelled

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export', // Outputs a Single-Page Application (SPA). // TODO: Remove this after rebuild
-  distDir: './dist', // Changes the build output directory to `./dist/`.
+  // distDir: './dist', // Changes the build output directory to `./dist/`.
 }
  
 export default nextConfig
