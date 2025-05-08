29a6b016455fa7ce8849dfd279f5c19d52cae66b
zambar <zambar@shanghaitech.edu.cn>
2025-05-08 17:47:39 +0800

    feat: new description

--- src/data/cats.ts
+++ src/data/cats.ts
@@ -2,10 +2,23 @@ import { ICat } from "@/models/cat";
 
 export const dummyCats: ICat[] = [
   {
+    avatar: "white",
+    name: "砂糖",
+    description: "嗯…这周也要继续加油喵！",
+    type: 0,
+    happiness: 0.9,
+    hunger: 0.4,
+    owned: true,
+    x: "0",
+    y: "0",
+    taskState: 0,
+    userId: ""
+  },
+  {
     avatar: "black",
     name: "煤炭",
-    description: "好黑啊，像煤炭一样",
-    type: 0,
+    description: "你好…数分作业明天就要交了…你没有忘记就好了。",
+    type: 1,
     happiness: 0.5,
     hunger: 0.2,
     owned: true,
@@ -17,8 +30,8 @@ export const dummyCats: ICat[] = [
   {
     avatar: "xl",
     name: "豆沙包",
-    description: "味道怎么样呢～",
-    type: 1,
+    description: "什么论文？你要写完那个论文才能来陪我吗…那你加油写哦！",
+    type: 2,
     happiness: 0.8,
     hunger: 0.6,
     owned: false,
@@ -29,8 +42,9 @@ export const dummyCats: ICat[] = [
   },
   {
     avatar: "orange",
-    name: "胖橘",
-    type: 2,
+    name: "元宝",
+    description: "这周有作业，下周也有作业，总是一个一个做就好了！",
+    type: 3,
     happiness: 0.8,
     hunger: 0.1,
     owned: true,
@@ -41,8 +55,9 @@ export const dummyCats: ICat[] = [
   },
   {
     avatar: "blue",
-    name: "蓝猫",
-    type: 3,
+    name: "水晶",
+    description: "这周完成作业的速度比之前快了24%，可喜可贺喵",
+    type: 4,
     happiness: 0.1,
     hunger: 0.9,
     owned: false,
@@ -53,8 +68,9 @@ export const dummyCats: ICat[] = [
   },
   {
     avatar: "pattern",
-    name: "花岗岩",
-    type: 4,
+    name: "瓷砖",
+    description: "突然想到了好主意…！要不我们现在去做那个作业吧～",
+    type: 5,
     happiness: 0.7,
     hunger: 0.3,
     owned: false,
@@ -64,20 +80,9 @@ export const dummyCats: ICat[] = [
     userId: ""
   },
   {
-    avatar: "white",
-    name: "白猫",
-    type: 5,
-    happiness: 0.9,
-    hunger: 0.4,
-    owned: true,
-    x: "0",
-    y: "0",
-    taskState: 0,
-    userId: ""
-  },
-  {
     avatar: "gray",
-    name: "灰猫",
+    name: "尘尘",
+    description: "早就提醒你啦，哼唧",
     type: 6,
     happiness: 0.6,
     hunger: 0.5,
@@ -90,7 +95,7 @@ export const dummyCats: ICat[] = [
   {
     avatar: "yellow",
     name: "罗勒",
-    description: "是男孩子哦",
+    description: "让我看看…接下来几天日程有点紧张呢！",
     type: 7,
     happiness: 0.8,
     hunger: 0.2,
